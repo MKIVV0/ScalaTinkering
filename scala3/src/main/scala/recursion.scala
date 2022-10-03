7d55fd8975a36866041f039b134199fa291df34a
Matt <huangzhangying@live.it>
2022-10-03 22:25:09 +0200

    Modified class recursion.scala Added gcd and fib

--- scala3/src/main/scala/recursion.scala
+++ scala3/src/main/scala/recursion.scala
@@ -7,6 +7,18 @@ def factorial_it(product : Int, counter : Int, max_counter : Int) : Int =
   if (counter > max_counter) product
   else factorial_it(product * counter, counter + 1, max_counter)
 
+// Recursive greatest common denominator
+def gcd(a : Int, b : Int) : Int =
+  if (b == 0) a else gcd(b, a % b)
+
+// Recursive fibonacci
+def fib(n : Int) : Int =
+  if (n == 0) 0
+  else if (n == 1) 1
+  else fib(n-1)+fib(n-2)
+
 @main def main(args : String) : Unit =
   var a = factorial_it(1, 1, 3)
   println("Iterative factorial: " + a)
+  println("GCD: " + gcd(10, 5))
+  println("Fibonacci: " + fib(3))
